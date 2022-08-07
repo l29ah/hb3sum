2c78a6ae013e64bf691841236c016aec5129ef02
Sergey Alirzaev <l29ah@riseup.net>
2022-08-07 11:52:54 +0200

    support non-mmap'able inputs

--- Main.hs
+++ Main.hs
@@ -4,14 +4,16 @@ module Main where
 
 import BLAKE3
 import Control.Monad
+import qualified Data.ByteString as B
 import System.Environment
 import System.IO.MMap
 
 main :: IO ()
 main = do
 	args <- getArgs
-	let files = if (length args) > 0 then args else error "Usage: hb3sum <files>"
+	let files = if (length args) > 0 then args else ["/dev/stdin"]
 	forM_ files $ \file -> do
-		bs <- mmapFileByteString file Nothing
+		bsMapped <- mmapFileByteString file Nothing
+		bs <- if B.length bsMapped > 0 then pure bsMapped else B.readFile file
 		let digest :: Digest 32 = hash [bs]
 		putStrLn (show digest ++ "\t" ++ file)
